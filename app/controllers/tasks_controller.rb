3faa672341f88c3beaf3a3c48c48e9e3ed24e5da
tadahiro.ogata <moremonkeymoney@gmail.com>
2021-05-23 10:08:08 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [ :show, :edit, :update, :destroy ]
     before_action :require_user_logged_in, only: [:index, :show, :edit]
+    before_action :correct_user, only: [:show, :create, :destroy]
     
     def index
         if logged_in?
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   
-  get 'user', to: 'users#index'
-  
+  get 'user', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
@@ -10,5 +9,5 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   
   resources :tasks
-  resources :users, only: [:create]
+  resources :users, only: [:index, :show, :create]
 end

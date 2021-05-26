0b5550d0d3f86f19b7c2e622fbd0da4f892a96e1
tadahiro.ogata <moremonkeymoney@gmail.com>
2021-05-26 08:50:19 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,13 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [ :show, :edit, :update, :destroy ]
     before_action :require_user_logged_in
-    before_action :correct_user, only: [:show, :destroy]
+    # before_action :correct_user, only: [:show, :destroy]
     
     def index
-        if logged_in?
-            @task = current_user.tasks.build
-            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-        end
+        # if logged_in?
+        #     @task = current_user.tasks.build
+        #     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        # end
         
         @tasks = Task.all
     end
@@ -20,7 +20,7 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
+        # @task = Task.new(task_params)
         @task = current_user.tasks.build(task_params)
         
         if @task.save
@@ -29,7 +29,7 @@ class TasksController < ApplicationController
         else
             @tasks = current_user.tasks.order(id: :desc).page(params[:page])
             flash.now[:danger] = 'taskが追加されませんでした'
-            render :login_url
+            render 'tasks/index'
         end
     end
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
 
+  get 'users', to: 'users#new'
+  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
@@ -8,5 +10,5 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   
   resources :tasks
-  resources :users, only: [:index, :create]
+  resources :users, only: [:create]
 end
